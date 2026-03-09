1f87e94df21ef214d0a5147448e549f195a119d6
Ben Burwood <77846694+ben-burwood@users.noreply.github.com>
2026-03-09 21:17:16 +0000

    Fix Port in SERVER_URL

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,6 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './style.css';
 
-export const SERVER_URL = `${window.location.protocol}//${window.location.hostname}:8080`;
+export const SERVER_URL = `${window.location.protocol}//${window.location.hostname}:${window.location.port}`;
 
 createApp(App).mount('#app')
